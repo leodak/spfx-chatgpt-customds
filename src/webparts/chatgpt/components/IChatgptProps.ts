fa3269a8e73abe61fdb0f427b3c7d273f795d457
Arun Kumar Durai <arun.kumar19688@gmail.com>
2023-04-27 11:29:57 +1200

    Update IChatgptProps.ts

--- src/webparts/chatgpt/components/IChatgptProps.ts
+++ src/webparts/chatgpt/components/IChatgptProps.ts
@@ -1,7 +1,15 @@
-export interface IChatgptProps {
+import { WebPartContext } from "@microsoft/sp-webpart-base";
+
+export interface IChatGptProps {
   description: string;
   isDarkTheme: boolean;
   environmentMessage: string;
   hasTeamsContext: boolean;
   userDisplayName: string;
+  wpContext: WebPartContext;
+}
+
+export interface IChatGPTState {
+  question: string;
+  answer: string;
 }
