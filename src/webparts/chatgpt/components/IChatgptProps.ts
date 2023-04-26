16fcaab76a08b24910bdc48228b0838928ecc2af
leodak <arun.kumar19688@gmail.com>
2023-04-27 10:55:52 +1200

    initial commit

---
+++ src/webparts/chatgpt/components/IChatgptProps.ts
@@ -0,0 +1,7 @@
+export interface IChatgptProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/chatgpt/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IChatgptWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'ChatgptWebPartStrings' {
+  const strings: IChatgptWebPartStrings;
+  export = strings;
+}
